ee1a2da9d5c6633895a803586df3bf8f3ca50424
taylorzhangyx <taylorzhangyx@yahoo.com>
2019-05-10 23:35:16 -0700

    Finish function chapter

--- Chapter1_LanguageFeature/Functions.ts
+++ Chapter1_LanguageFeature/Functions.ts
@@ -29,3 +29,36 @@ const collectionresult = finderInst.FindEntryByCollection('document');
 
 // Arrow Function
 const arrowFunction1 = (a: number, b: number) => a + b;
+const arrowFunctionresult1 = arrowFunction1(1, 2);
+
+const arrowReturnObject = (a: number, b: number) => ({ a: a, b: b });// This will return an object
+
+const scopeLosingExample = {
+    text: 'property from loasing scope',
+    run: function () {
+        setTimeout(function () {
+            // console.log(this.text);
+        }, 1000);
+    }
+}
+
+const scopePersistingExample = {
+    text: 'property from persisting scope',
+    run: function () {
+        setTimeout(() => {
+            console.log(this.text);
+        }, 1000);
+    }
+}
+
+//Funtion Currying
+const multiply = (a: number) => (b: number) => a * b;
+const numberA = multiply(2)(3); //6
+const multiplyBy10 = multiply(10);
+const num100 = multiplyBy10(10);
+const num1000 = multiplyBy10(num100);
+//currying usage on logging
+const log = (source: string) => (message: string) => console.log(source, message);
+const userloger = log('ZhangYuxin logged:');
+userloger('message1');
+userloger('message2');
